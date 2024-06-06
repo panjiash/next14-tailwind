676f77b597b17fff077838364beb61e41e4c5eda
panjiash <pashriyandhi@gmail.com>
2024-06-06 17:44:55 +0700

    vip code studio next js app router eps 17

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -91,6 +91,21 @@ export default function LoginPage() {
             >
               {isLoading ? "Sign In..." : "Login to your account"}
             </button>
+            <hr />
+            <button
+              type="button"
+              onClick={() =>
+                signIn("google", { callbackUrl: "/dashboard", redirect: false })
+              }
+              disabled={isLoading}
+              className={
+                !isLoading
+                  ? "w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  : "w-full text-white bg-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600"
+              }
+            >
+              {isLoading ? "Sign In..." : "Login with google"}
+            </button>
             <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
               Not registered?{" "}
               <Link
---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import withAuth from "./middleware/withAuth";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+export function mainMiddleware(request: NextRequest) {
+  const res = NextResponse.next();
+  return res;
+}
+
+export default withAuth(mainMiddleware, [
+  "/dashboard",
+  "/profile",
+  "/login",
+  "/register",
+]);
