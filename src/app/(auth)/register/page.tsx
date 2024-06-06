d3fa931c67654bdfbb95cd8a5fc426c5d29e0fbe
panjiash <pashriyandhi@gmail.com>
2024-06-06 15:04:58 +0700

    update: adding register service with firebase database

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,11 +1,43 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function RegisterPage() {
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError("");
+    setIsLoading(true);
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+
+    if (res.status === 200) {
+      e.target.reset();
+      push("/login");
+      setIsLoading(false);
+    } else {
+      setError("Email already exist!");
+      setIsLoading(false);
+    }
+  };
   return (
     <>
-      <div className="h-screen w-100 flex justify-center items-center">
+      <div className="h-screen w-100 flex justify-center items-center flex-col">
+        {error !== "" && (
+          <div className="text-red-600 font-bold mb-3">{error}</div>
+        )}
         <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-          <form className="space-y-6" action="#">
+          <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
             <h3 className="text-xl font-medium text-gray-900 dark:text-white">
               Sign up to our platform
             </h3>
@@ -62,9 +94,14 @@ export default function RegisterPage() {
 
             <button
               type="submit"
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className={
+                !isLoading
+                  ? "w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+                  : "w-full text-white bg-gray-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600  cursor-not-allowed"
+              }
+              disabled={isLoading}
             >
-              Sign Up to your account
+              {isLoading ? "Loading..." : "Sign up account"}
             </button>
             <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
               Have registered?{" "}
